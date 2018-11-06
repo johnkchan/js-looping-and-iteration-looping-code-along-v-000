02087eec6282d2866d6077d2d62268ca83d3f6a5
Jonathan Chan <johnkchan252@gmail.com>
2018-11-06 03:41:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ function printBadges(employees){
   return employees;
 }
 
-function tailsNeverFails(){
-  
+function tailsNeverFails () {
+  let tails = 0;
+
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+
+  return `You got ${tails} tails in a row!`;
 }
