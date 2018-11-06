2b1b434d6f5542e58b66293a93a54ce169714a9a
Jonathan Chan <johnkchan252@gmail.com>
2018-11-06 03:36:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 function printBadges(employees){
   for (let i = 0; i < employees.length; i++){
-    console.log("LOG: Welcome",employees[i], "! You are employee #" + i);
+    console.log("LOG: Welcome " + employees[i] + "! You are employee #" + i);
   }
 }
